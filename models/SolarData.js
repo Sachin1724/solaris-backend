140f1769f168f095153815ea9c0d03f23f002838
Sachin1724 <fakesachin.19@gmail.com>
2025-10-31 23:28:25 +0530

    update solaris data

--- models/SolarData.js
+++ models/SolarData.js
@@ -1,3 +1,4 @@
+// models/SolarData.js
 const mongoose = require("mongoose");
 
 const SolarDataSchema = new mongoose.Schema(
@@ -5,9 +6,14 @@ const SolarDataSchema = new mongoose.Schema(
     temperature: Number,
     humidity: Number,
     dustVoltage: Number,
-    dustDensity: Number,
-    ldrLeft: Number,
-    ldrRight: Number,
+
+    // --- CORRECTIONS ---
+    // These fields now match what server.js provides
+    dust: Number,       // CHANGED from dustDensity
+    ldr: Number,        // CHANGED from ldrLeft
+    ldrPercent: Number, // CHANGED from ldrRight
+    // -------------------
+
     voltage: Number,
     current: Number,
     power: Number,
